8891c58fd206ad3833818c747b71c5f95ade79e3
JmlKhan <jmlkhon17@gmail.com>
2022-01-08 14:18:32 +0500

    add validation

--- Task/Controllers/HomeController.cs
+++ Task/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Task.Models;
 using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 
+
 public class HomeController : Controller
 {
 
@@ -44,62 +45,70 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult Index(IFormFile postedFile)
     {
-        
 
-
-        if (postedFile != null)
+        try
         {
-            string path = Path.Combine(Environment.WebRootPath, "Uploads");
-            if (!Directory.Exists(path))
+            if (postedFile != null)
             {
-                Directory.CreateDirectory(path);
-            }
-
-            string fileName = Path.GetFileName(postedFile.FileName);
-            string filePath = Path.Combine(path, fileName);
-
-            using (FileStream stream = new FileStream(filePath, FileMode.Create))
-            {
-                postedFile.CopyTo(stream);
-            }
-
-            //reading data from file
-            List<string> csvData = System.IO.File.ReadAllLines(filePath)
-                .Skip(1)
-                .Where(line => line.Length > 1)
-                .ToList();           
-
-            //inserting csv data into sql database 
-            foreach (var row in csvData)
-            {
-                if (!string.IsNullOrEmpty(row))
+                string path = Path.Combine(Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
                 {
-                    var cells = row.Split(',');
-                    var emp = new Employee
-                    {
-                        Payroll_Number = cells[0],
-                        Forenames = cells[1],
-                        Surname = cells[2],
-                        Date_of_Birth = DateTime.ParseExact(cells[3], "d/M/yyyy", CultureInfo.InvariantCulture),
-                        Telephone = int.Parse(cells[4]),
-                        Mobile = int.Parse(cells[5]),
-                        Address = cells[6],
-                        Address_2 = cells[7],
-                        Postcode = cells[8],
-                        Email_Home = cells[9],
-                        Start_Date = DateTime.ParseExact(cells[10], "dd/MM/yyyy", CultureInfo.InvariantCulture)
-
-                    };
-
-                    _context.Add(emp);
-                    _context.SaveChanges();
+                    Directory.CreateDirectory(path);
                 }
 
-            }           
-           
-            return RedirectToAction("Index");
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                //reading data from file
+                List<string> csvData = System.IO.File.ReadAllLines(filePath)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .ToList();
+
+                //inserting csv data into sql database 
+                foreach (var row in csvData)
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var cells = row.Split(',');
+                        var emp = new Employee
+                        {
+                            Payroll_Number = cells[0],
+                            Forenames = cells[1],
+                            Surname = cells[2],
+                            Date_of_Birth = DateTime.ParseExact(cells[3], "d/M/yyyy", CultureInfo.InvariantCulture),
+                            Telephone = int.Parse(cells[4]),
+                            Mobile = int.Parse(cells[5]),
+                            Address = cells[6],
+                            Address_2 = cells[7],
+                            Postcode = cells[8],
+                            Email_Home = cells[9],
+                            Start_Date = DateTime.ParseExact(cells[10], "dd/MM/yyyy", CultureInfo.InvariantCulture)
+
+                        };
+
+                        _context.Add(emp);
+                        _context.SaveChanges();
+                    }
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("NoData");
         }
-        return RedirectToAction("NoData");
+        catch (Exception ex)
+        {
+
+            return BadRequest($"{ex.Message} \nTry again with correct file format!!!");
+        }
+
+       
     }
     [HttpGet]
     public ActionResult NoData()
--- Task/Models/Employee.cs
+++ Task/Models/Employee.cs
@@ -5,16 +5,38 @@ namespace Task.Models
     public class Employee
     {
         [Key]
+        [DataType(DataType.Text)]
+        [Required]
         public string Payroll_Number { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
         public string Forenames { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
         public string Surname { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
         public DateTime Date_of_Birth { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public int Telephone { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public int Mobile { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
         public string Address { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
         public string Address_2 { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
         public string Postcode { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string Email_Home { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
         public DateTime Start_Date { get; set; }
 
 
